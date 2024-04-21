ed805954f8f470247a8e1ebd21d5a3a553038278
Ignacio Zamo <ignaciozamo1@gmail.com>
2024-04-21 16:24:37 -0300

    se aprende funcion onclick

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import Events from "../src/components/eventos"
 
 function App() {
 
+  const funcionBoton = ()=>{
+    alert("me clickeaste!")
+  }
 
   return (
     <>
+      <button onClick={funcionBoton}>clickeame!</button>
       <Navbar/>
       <Events/>
     </>
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,10 +1,22 @@
-const Navbar = ()=>{
-    return (
-        <div>
-            <p>Eventos</p>
-            <input type="text" placeholder="Busca tu evento favorito" />
-        </div>
-    )
-}
+import { useState } from "react"
 
-export default Navbar
+    const NavBar = ()=>{
+        const [search, setSearch] = useState('');
+        const handleInputChange = (e)=>{
+            setSearch(e.target.value)
+        };
+
+        console.log(search)
+        
+        return (
+         <div>
+             <p>Eventos</p>
+             <input type="text"
+                    placeholder="Busca tu evento favorito" 
+                    onChange = {handleInputChange}
+                    value = {search}/>
+         </div>
+     )
+    }
+
+    export default NavBar;
