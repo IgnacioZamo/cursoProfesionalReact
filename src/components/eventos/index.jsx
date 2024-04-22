8e07a63386f7f9b77051c764e347efec260bc487
Ignacio Zamo <ignaciozamo1@gmail.com>
2024-04-22 20:12:04 -0300

    Se manda props por un evento

--- src/components/eventos/index.jsx
+++ src/components/eventos/index.jsx
@@ -3,12 +3,18 @@ import data from "../../data/events.json"
 
 const {_embedded: {events}} = data;
 
+const handleEventItemClick = (id)=>{
+    alert(`Evento clickeado: ${id}`)
+}
+
 const eventsComponentes = events.map((eventItem) => (
     <EventItem 
         key={`event-item-${eventItem.id}`} 
         name = {eventItem.name}
         info={eventItem.info}
         img={eventItem.images[0].url}
+        onEventClick={handleEventItemClick}
+        id = {eventItem.id}
     /> ))
 
 const Events = ()=>{
--- src/components/eventos/itemEvento/index.jsx
+++ src/components/eventos/itemEvento/index.jsx
@@ -1,9 +1,12 @@
-const Events = ({info, name, img })=>{
+const Events = ({id, info, name, img, onEventClick })=>{
     return (
-        <div>
+        <div >
             <h4>{name}</h4>
             <p>{info}</p>
-            <img src={img} alt={`Imagen relacionada a: ${name}`} height={200} />
+            <img src={img} alt={`Imagen relacionada a: ${name}`} height={200} /> <br />
+            <button onClick={(e)=>{
+                e.stopPropagation(); //para que el evento no se propage por el padre
+                onEventClick (id)}} >Mas info</button>
         </div>
     )
 }
