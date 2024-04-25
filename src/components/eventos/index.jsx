624e8e77fde4e0bfa6d6d80932af1bc6ccf82873
Ignacio Zamo <ignaciozamo1@gmail.com>
2024-04-24 21:55:26 -0300

    Se termina navbar de busqueda

--- src/components/eventos/index.jsx
+++ src/components/eventos/index.jsx
@@ -2,28 +2,30 @@ import EventItem from "./itemEvento"
 import data from "../../data/events.json"
 
 
-
-const {_embedded: {events}} = data;
-
-const handleEventItemClick = (id)=>{
-    alert(`Evento clickeado: ${id}`)
-}
-
-const renderEventsComponentes = ()=>{
-    let eventFilter = events;
-    return events.map((eventItem) => (
-        <EventItem 
-            key={`event-item-${eventItem.id}`} 
-            name = {eventItem.name}
-            info={eventItem.info}
-            img={eventItem.images[0].url}
-            onEventClick={handleEventItemClick}
-            id = {eventItem.id}
-        /> ))
-
-} 
-
-const Events = ()=>{
+const Events = ({searchValue})=>{
+    const {_embedded: {events}} = data;
+    
+    const handleEventItemClick = (id)=>{
+        alert(`Evento clickeado: ${id}`)
+    }
+    
+    const renderEventsComponentes = ()=>{
+        let eventFilter = events;
+        if (searchValue.length > 0) {
+            eventFilter = eventFilter.filter((item)=> item.name.toLocaleLowerCase().includes(searchValue))
+        }
+    
+        return eventFilter.map((eventItem) => (
+            <EventItem 
+                key={`event-item-${eventItem.id}`} 
+                name = {eventItem.name}
+                info={eventItem.info}
+                img={eventItem.images[0].url}
+                onEventClick={handleEventItemClick}
+                id = {eventItem.id}
+            /> ))
+    
+    } 
     return(
         <div>
             {renderEventsComponentes()}
